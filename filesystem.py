e0a580a1559f21db5db4755f654dc1785f6dce45
Graham Rogozinski <ggr4460@rit.edu>
2024-11-12 18:53:21 -0500

    built repo

--- filesystem.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def analyze_filesystem(instance_id):
-    logging.info(f"Analyzing filesystem of instance {instance_id}")
-    # Logic to collect and analyze filesystem data
--- utils.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def setup_logging(level=logging.INFO):
-    logging.basicConfig(level=level)
-    logging.info("Logging setup complete.")
